a91dae6d6503d4c75f1fc52d6548824fcd961321
Amphiluke <amphiluke@gmail.com>
2013-06-04 00:40:37 +0700

    Prevent fix-ie-css-limit processing in IE 10

--- src/fix-ie-css-limit/fix-ie-css-limit-standalone.js
+++ src/fix-ie-css-limit/fix-ie-css-limit-standalone.js
@@ -13,50 +13,51 @@
 
 (function () {
 
-    if (document.styleSheets.length < (window.cssFixCountIE || 31)) return;
+	@if (@_jscript_version >= 10) return; @end
+	if (document.styleSheets.length < (window.cssFixCountIE || 31)) return;
 
-    var links,
-        paths = [],
-        importsPerStyle = 15, // a number of @import directives per one CSS file
-        tmpA = document.createElement("a"),
-        sheet, i, len, media, href, remain, allLinks;
+	var links,
+		paths = [],
+		importsPerStyle = 15, // a number of @import directives per one CSS file
+		tmpA = document.createElement("a"),
+		sheet, i, len, media, href, remain, allLinks;
 
-    if (typeof document.querySelectorAll == "function") { // IE 9+
-        links = Array.prototype.slice.call(document.querySelectorAll("link[rel='stylesheet']"));
-    } else { // IE <9
-        links = [];
-        allLinks = document.getElementsByTagName("link");
-        for (i = 0, len = allLinks.length; i < len; i++) {
-            if (allLinks[i].rel.toLowerCase() == "stylesheet") {
-                links.push(allLinks[i]);
-            }
-        }
-    }
-    len = links.length;
-    for (i = 0; i < len; i++) {
-        href = links[i].href || "";
-        media = links[i].media || "";
-        if (href && media.indexOf("print") == -1) {
-             // workaround for obtaining an absolute URL (independent of where the @import directive will be placed)
-            tmpA.href = href;
-            paths.push(tmpA.href);
-        }
-    }
-    remain = Math.ceil(paths.length / importsPerStyle);
-    for (i = 0; i < len; i++) {
-        if (i < remain) {
-            sheet = links[i].styleSheet;
-            if (sheet && sheet.cssText != "") {
-                sheet.cssText = "";
-            }
-        } else {
-            links[i].parentNode.removeChild(links[i]);
-        }
-    }
-    links.splice(remain);
-    for (i = 0, len = paths.length; i < len; i++) {
-        links[Math.floor(i / importsPerStyle)].styleSheet.addImport(paths[i]);
-    }
+	if (typeof document.querySelectorAll == "function") { // IE 9+
+		links = Array.prototype.slice.call(document.querySelectorAll("link[rel='stylesheet']"));
+	} else { // IE <9
+		links = [];
+		allLinks = document.getElementsByTagName("link");
+		for (i = 0, len = allLinks.length; i < len; i++) {
+			if (allLinks[i].rel.toLowerCase() == "stylesheet") {
+				links.push(allLinks[i]);
+			}
+		}
+	}
+	len = links.length;
+	for (i = 0; i < len; i++) {
+		href = links[i].href || "";
+		media = links[i].media || "";
+		if (href && media.indexOf("print") == -1) {
+			 // workaround for obtaining an absolute URL (independent of where the @import directive will be placed)
+			tmpA.href = href;
+			paths.push(tmpA.href);
+		}
+	}
+	remain = Math.ceil(paths.length / importsPerStyle);
+	for (i = 0; i < len; i++) {
+		if (i < remain) {
+			sheet = links[i].styleSheet;
+			if (sheet && sheet.cssText != "") {
+				sheet.cssText = "";
+			}
+		} else {
+			links[i].parentNode.removeChild(links[i]);
+		}
+	}
+	links.splice(remain);
+	for (i = 0, len = paths.length; i < len; i++) {
+		links[Math.floor(i / importsPerStyle)].styleSheet.addImport(paths[i]);
+	}
 
 })();
 
